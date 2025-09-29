40931f878380c2c335f4f375dcf3223cbd2c851d
mmws <info@makemywebspace.com>
2025-09-29 10:51:47 -0500

    nfl_stacks.py

--- nfl_stacks.py
+++ nfl_stacks.py
@@ -213,10 +213,18 @@ optimizer.set_max_repeating_players(max_repeating_players)
 st.markdown("### Stacking Options")
 enable_qb_wr = st.checkbox("QB + WR stack", value=True)
 enable_qb_te = st.checkbox("QB + TE stack", value=False)
+enable_qb_rb_wr = st.sidebar.checkbox("QB + RB + WR Stack", value=False)
+enable_qb_rb_te = st.sidebar.checkbox("QB + RB + TE Stack", value=False)
+enable_qb_wr_wr = st.sidebar.checkbox("QB + WR + WR Stack", value=False)
+enable_qb_te_wr = st.sidebar.checkbox("QB + TE + WR Stack", value=False)
 enable_team_stack = st.checkbox("Team stack (3 players: QB/WR/TE)", value=True)
 enable_game_stack = st.checkbox("Game stack (4 players, min 1 from opponent)", value=False)
 no_double_rb = st.checkbox("Restrict 2 RBs from same team", value=True)
-force_qb_bringback = st.checkbox("Force QB vs WR/RB bring-back", value=False)
+optimizer.add_stack(GameStack(
+    positions=[("QB",)],                # primary stack side
+    opposing_positions=[("QB", "RB", "WR", "TE")],  # allow any bringback
+    max_exposure=1.0
+))
 
 gen_btn = st.button("Generate lineups")
 
